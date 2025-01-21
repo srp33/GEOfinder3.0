f76751f1d43eb86b00034c45ec6389ac946c7c26
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2025-01-21 13:27:36 -0700

    Uncommented first command

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-# docker run -i -t --rm \
-#     -v "$(pwd)":/app \
-#     --user $(id -u):$(id -g) \
-#     srp33/geofinder \
-#         python make_filtered_tsv.py
+docker run -i -t --rm \
+    -v "$(pwd)":/app \
+    --user $(id -u):$(id -g) \
+    srp33/geofinder \
+        python make_filtered_tsv.py
 
 rm -rf collectionFiles
 
