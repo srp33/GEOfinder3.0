aca5f82b98a76593149cf2b60ef847fbca42529b
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2025-01-21 13:04:16 -0700

    Added line counter

--- make_collection.py
+++ make_collection.py
@@ -5,9 +5,14 @@ import gzip
 chroma_client = chromadb.PersistentClient(path="./collectionFiles")
 geo_collection = chroma_client.create_collection(name="geo_collection")
 
+lineCount = 1
+
 with gzip.open("tsvFiles/gte-large.tsv.gz") as gse_emb_file:
     for line in gse_emb_file:
-        
+        if lineCount % 100:
+            print(f"Processed line {lineCount}")
+        lineCount += 1
+
         line_items = line.decode().rstrip("\n").split("\t")
         gse = line_items[0]
         embedding = line_items[1:]
