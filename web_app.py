3b56adceaee8c44c48b46133c5af6306f8d975d3
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2025-01-29 08:48:26 -0700

    Modified it to use geofinder in URL

--- web_app.py
+++ web_app.py
@@ -4,12 +4,12 @@ import error_msg
 import helper
 import os
 import pandas as pd
-import re 
-import traceback 
+import re
+import traceback
 
 #make global data frames
 global data_frame
-data_frame = pd.read_csv("tsvFiles/filtered_AllGEO.tsv.gz", sep="\t") 
+data_frame = pd.read_csv("tsvFiles/filtered_AllGEO.tsv.gz", sep="\t")
 
 all_geo_ids = pd.read_csv("tsvFiles/filtered_AllGEO_ids.tsv.gz", sep="\t")
 gte_ids = pd.read_csv("tsvFiles/gte_ids.tsv.gz", sep="\t")
@@ -31,23 +31,23 @@ class WebApp:
                 traceback.print_exc(file=error_file)
 
             return error_msg.render_error()
-    
+
     @cherrypy.expose
     def about(self):
         return self.head() + self.read_text_file("./htmlFiles/about.html") + self.footer()
-    
+
     @cherrypy.expose
     def head(self):
         return self.read_text_file("./htmlFiles/head.html")
-    
+
     @cherrypy.expose
     def search_home(self):
         return self.read_text_file("./htmlFiles/search_home.html")
-    
+
     @cherrypy.expose
     def footer(self):
         return self.read_text_file("./htmlFiles/footer.html")
-    
+
     # a) 1-10, b) 11-50, c) 51-100, d) 101-500, e) 501-1000, f) 1000+
     @cherrypy.expose
     def query(self, ids, a="", b="", c="", d="", e="", f="", rnaSeq="", microarr="", startYear="", endYear=""):
@@ -80,9 +80,9 @@ class WebApp:
     def bottom_half_html(self, ids, metadata_dct):
         return f"""
                     <div id="results">
-                        {self.validate_input(ids, metadata_dct)}      
+                        {self.validate_input(ids, metadata_dct)}
                     </div>
-                    
+
                     <script>
                         $('#submitButton').prop('disabled', false);
                         document.getElementById('results').scrollIntoView({{ behavior: "smooth", block: "start" }});
@@ -91,17 +91,17 @@ class WebApp:
             </html>
             """
 
-    #checks for invalid ID input, if all input is valid then calls generate_rows 
+    #checks for invalid ID input, if all input is valid then calls generate_rows
     def validate_input(self, ids, metadata_dct):
         if (ids == ""):
-            return ""  
+            return ""
         elif (self.validate_checkboxes(metadata_dct)):
-            return error_msg.checkbox_error_msg() 
+            return error_msg.checkbox_error_msg()
         elif (self.validate_years(metadata_dct)):
-            return self.validate_years(metadata_dct) 
-        
+            return self.validate_years(metadata_dct)
+
         return self.validate_ids(ids, metadata_dct)
-        
+
     def validate_ids(self, ids, metadata_dct):
         id_lst = re.split(r"\n|,",ids.strip())
         bad_format_ids = []
@@ -115,26 +115,26 @@ class WebApp:
                 bad_format_ids.append(id)
             elif id not in database_ids:
                 not_found_ids.append(id)
-            else: 
+            else:
                 valid_ids.append(id)
 
         # If all entered IDs and years were valid, call generate_rows to get results
         if valid_ids:
             if bad_format_ids or not_found_ids:
                 return error_msg.invalid_input_msg(bad_format_ids, not_found_ids, valid_ids) + WebApp.generate_rows(valid_ids=valid_ids, metadata_dct=metadata_dct)
-            else: 
+            else:
                 return WebApp.generate_rows(valid_ids=valid_ids, metadata_dct=metadata_dct)
         else:
             return error_msg.invalid_input_msg(bad_format_ids, not_found_ids, valid_ids)
-            
+
     def validate_checkboxes(self, metadata_dct):
         #make sure some boxes are checked
         if metadata_dct["Num_Samples"] == [] or metadata_dct["Experiment_Type"] == []:
             return True
         return False
-        
+
     def validate_years(self, metadata_dct):
-        #validates year input          
+        #validates year input
         bad_format_years = []
         not_found_years = []
         valid_years = []
@@ -151,19 +151,19 @@ class WebApp:
         if int(endYear)<2001 or int(endYear)>2024:
             not_found_years.append(endYear)
 
-        if(bad_format_years==[] and not_found_years==[]):     
+        if(bad_format_years==[] and not_found_years==[]):
             if(int(endYear) >= int(startYear)):
                 valid_years.append(startYear)
                 valid_years.append(endYear)
             else:
                 bad_format_years.append(startYear)
                 bad_format_years.append(endYear)
-        
+
         if bad_format_years or not_found_years:
-            return error_msg.invalid_year_msg(bad_format_years, not_found_years, valid_years) 
+            return error_msg.invalid_year_msg(bad_format_years, not_found_years, valid_years)
         return False
-        
-    #calls generate_query_results and writes results in html code, to display results in a table 
+
+    #calls generate_query_results and writes results in html code, to display results in a table
     def generate_rows(valid_ids=[], metadata_dct={}):
 
         filtered_df = helper.filter_by_metas(metadata_dct)
@@ -172,14 +172,14 @@ class WebApp:
         #queries the collection to get most similar results based on user's valid ID's
         if valid_ids:
             results_ids = helper.generate_id_query_results(valid_ids)
-        
+
         #creates a list of ID's, where the filtered ID's and query ID's overlap
         match_ids = [value for value in results_ids if value in filtered_ids]
 
         results_df = filtered_df[filtered_df["GSE"].isin(match_ids)]
         results_df = results_df.reset_index(drop=True)
-        
-        # start of process to convert the dataframe to a javascript array 
+
+        # start of process to convert the dataframe to a javascript array
         # table_info = results_df.to_dict(orient='records')
         # json.dumps(table_info)
 
@@ -211,18 +211,18 @@ class WebApp:
                 <td>{line["SubSeries_GSE"].values[0]}</td> </tr>'''
         table += f'''</tbody></table>'''
         return table
-    
+
 if __name__ == '__main__':
     style_path = os.path.abspath("styles.css")
     icon_path = os.path.abspath("geo_logo.ico")
-    pagination_path = os.path.abspath("pagination.js")
+    # pagination_path = os.path.abspath("pagination.js")
 
     cherrypy.config.update({
         'server.socket_host': '0.0.0.0',
         'server.socket_port': 8080,
     })
 
-    cherrypy.quickstart(WebApp(), "/", {
+    cherrypy.quickstart(WebApp(), "/geofinder", {
             '/styles.css':
             {
                 'tools.staticfile.on': True,
@@ -233,17 +233,17 @@ if __name__ == '__main__':
                 'tools.staticfile.on': True,
                 'tools.staticfile.filename': icon_path
             },
-            '/pagination.js':
-            {
-                'tools.staticfile.on': True,
-                'tools.staticfile.filename': pagination_path
-            }
+            # '/pagination.js':
+            # {
+            #     'tools.staticfile.on': True,
+            #     'tools.staticfile.filename': pagination_path
+            # }
     })
 
 '''
 TO-DO
 - later: pagination (idea: copy/paste contents from pagination.js straight into the script tag of the webapp)
-- Info box is not scrolling 
+- Info box is not scrolling
 - add an error message where either the number is not found in GEO at all, or "this accession ID is not currently supported by this web app"
 
 questions:
