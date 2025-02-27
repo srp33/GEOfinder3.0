1e32e18ac99578425267f31ce3d96f1767764c8e
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2025-02-27 13:56:35 -0700

    Changed port number

--- run_app.sh
+++ run_app.sh
@@ -4,6 +4,6 @@ docker run -i -t --rm \
     -v "$(pwd)":/app \
     --user $(id -u):$(id -g) \
     --name geofinder \
-    -p 8080:8080 \
+    -p 9005:8080 \
     srp33/geofinder \
         python web_app.py
